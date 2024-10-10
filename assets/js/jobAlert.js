cf6f8c6c220f2192dd4c289484660200f3c98541
Xavier Vitali <44967621+xaviervitali@users.noreply.github.com>
2024-10-10 16:42:42 +0200

    css again

--- assets/js/jobAlert.js
+++ assets/js/jobAlert.js
@@ -1,64 +1,10 @@
-import moment from "moment";
-import { generateDataTable } from "./datatable";
+import { performSearch } from "./search";
 
 document.addEventListener("DOMContentLoaded", function () {
     const tableDataSelector = document.querySelector(".js-table-data");
     const tableDataAdzuna = JSON.parse(tableDataSelector.getAttribute("data-table-adzuna"));
     const tableDataFranceTelecom = JSON.parse(tableDataSelector.getAttribute("data-table-france-telecom"));
     
-    let adzuneData = [];
-    let franceTelecomData = [];
-
-    tableDataAdzuna.forEach(job => {
-
-        const link = createLink(job.redirect_url)
-   
-        adzuneData.push({ company: job.company.display_name, location: job.location.display_name, description: job.description, title: job.title, link, created: moment(job.created).format('DD/MM/YY') })
-    })
-
-    tableDataFranceTelecom.forEach(job => {
-        
-        const link = createLink( job.contact?.urlPostulation ?? 'https://candidat.francetravail.fr/offres/recherche/detail/' + job.id)
-
-        franceTelecomData.push({
-            company: job.entreprise.nom ?? 'non renseigné',
-            // company_description: job.entreprise.description ?? 'non renseignée',
-            type_contrat: job.typeContratLibelle, location: formatFtLocation(job.lieuTravail.libelle), description: job.description, title: job.intitule, link, created: moment(job.dateCreation).format('DD/MM/YY')
-        })
-    })
-
-    generateDataTable(adzuneData, [
-        "company",
-        'created',
-        "title",
-        "description",
-        "location",
-        "link"], false, '#table_adzuna');
-    // debugger
-    generateDataTable(franceTelecomData, [
-        "company",
-        // "company_description",
-        'created',
-        "title",
-        "description",
-        "type_contrat",
-        "location",
-        "link"], false, "#table_ft");
+    document.getElementById("search-input-adzuna").addEventListener('keyup', e=>performSearch(e.target.value, tableDataAdzuna, 'job-list-adzuna', ['company', 'description']))
+    document.getElementById("search-input-ft").addEventListener('keyup', e=>performSearch(e.target.value, tableDataFranceTelecom, 'job-list-ft', ['company', 'description']))
 })
-
-function formatFtLocation(location){
-    return location.slice(4) + ' ('+location.slice(0, 2) +')'
-}
-
-function createLink(url) {
-    if (!url) {
-        return 'Allez sur le site';
-    }
-    const link = document.createElement('a');
-
-    link.href = url;
-    link.textContent = 'Visualiser';
-    link.target = "_blank"
-
-    return link
-}
--- assets/js/synthese.js
+++ assets/js/synthese.js
@@ -2,6 +2,7 @@ import { noActionLabel } from "../app.js";
 import "../styles/synthese.css";
 import { generatePieChart } from './chart.js';
 import { generateDataTable } from './datatable.js';
+import { performSearch } from "./search.js";
 
 document.addEventListener("DOMContentLoaded", function () {
 
@@ -19,32 +20,6 @@ document.addEventListener("DOMContentLoaded", function () {
   generatePieChart(labels, series)
 
   document.getElementById('search-input').addEventListener('keyup', e => performSearch(e.target.value, tableData))
-  // generateDataTable(tableData );
 
 });
 
-function performSearch(query, jobsInProgress) {
-  const jobList = document.getElementById('job-list');
-  jobList.innerHTML = ''; // Vider la liste actuelle
-
-  const filteredJobs = jobsInProgress.filter(job =>
-    job.recruiter.toLowerCase().includes(query.toLowerCase()) ||
-    job.description.toLowerCase().includes(query.toLowerCase())
-  );
-
-
-
-  const jobCardTemplate = document.getElementById('cardTemplate').innerHTML;
-  filteredJobs.forEach(job => {
-    let jobElement = jobCardTemplate;
-    for (const key in job) {
-      if (job.hasOwnProperty(key)) {
-        const regex = new RegExp(`job_${key}`, 'g');
-        const value =  typeof job[key]  === 'string' ? job[key].slice(0, 300) : job[key]
-        
-        jobElement = jobElement.replace(regex, value);
-      }
-    }
-    jobList.innerHTML += jobElement;
-  });
-}
